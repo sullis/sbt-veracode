443a874dbccce20a4d9a7230a1592921cc191d7f
Sean Sullivan <github@seansullivan.com>
2019-01-09 21:35:40 -0800

    add [src/sbt-test]

---
+++ src/sbt-test/sbt-veracode/min-project/build.sbt
@@ -0,0 +1,4 @@
+enablePlugins(VeracodePlugin)
+name := "min-project"
+
+scalaVersion := "2.12.8"
---
+++ src/sbt-test/sbt-veracode/min-project/src/main/scala/Hello.scala
@@ -0,0 +1,7 @@
+package minproject
+
+object Hello {
+  def main(args: Array[String]) = {
+    println("Hello World!!")
+  }
+}
