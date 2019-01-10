5a40528fa49adbb41d39412dc0950ca4af5b31f0
Sean Sullivan <github@seansullivan.com>
2019-01-09 20:45:17 -0800

    add taskKey's

--- src/main/scala/io/github/sullis/sbt/veracode/VeracodePlugin.scala
+++ src/main/scala/io/github/sullis/sbt/veracode/VeracodePlugin.scala
@@ -8,7 +8,23 @@ object VeracodePlugin extends AutoPlugin {
   val veracodeArtifact = settingKey[String]("artifact on the local filesystem")
   val veracodeAppName = settingKey[String]("application name")
 
+  val createBuild = taskKey[Unit]("createBuild")
+  val uploadFile = taskKey[Unit]("uploadFile")
+  val uploadFileSandbox = taskKey[Unit]("uploadFileSandbox")
+
   override lazy val projectSettings = Seq(
     veracodeAppName := sbt.Keys.name.value
   )
+
+  createBuild := {
+    System.out.println("createBuild")
+  }
+
+  uploadFile := {
+    System.out.println("uploadFile")
+  }
+
+  uploadFileSandbox := {
+    System.out.println("uploadFileSandbox")
+  }
 }
