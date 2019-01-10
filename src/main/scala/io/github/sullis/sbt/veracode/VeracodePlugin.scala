aac885be0641b04d162cefe47fb82560ceb293ab
Sean Sullivan <github@seansullivan.com>
2019-01-09 17:01:46 -0800

    cleanup settings

--- src/main/scala/io/github/sullis/sbt/veracode/VeracodePlugin.scala
+++ src/main/scala/io/github/sullis/sbt/veracode/VeracodePlugin.scala
@@ -3,10 +3,10 @@ package io.github.sullis.sbt.veracode
 import sbt._
 
 object VeracodePlugin extends AutoPlugin {
-  val veracodeApiKey = settingKey[String]("Veracode API key")
   val veracodeApiId = settingKey[String]("Veracode API id")
+  val veracodeApiKey = settingKey[String]("Veracode API key")
   val veracodeArtifact = settingKey[String]("artifact on the local filesystem")
-  val veracodeAppName = settingKey[String]("artifact on the local filesystem")
+  val veracodeAppName = settingKey[String]("application name")
 
   override lazy val projectSettings = Seq(
     veracodeAppName := sbt.Keys.name.value
